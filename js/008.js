ab087953e3d1c7a4ac33b8578f6ecf24a1010d11
vlobo <valentinlobo@hotmail.com>
2024-08-06 06:47:09 -0300

    efercicio 8 resuleto

---
+++ js/008.js
@@ -0,0 +1,21 @@
+/*8- Crea script para generar pirámide siguiente con los números del 1 al número que indique el usuario (no mayor de 50) 
+
+1
+12
+123
+1234
+12345
+123456
+
+*/
+
+for (let i = 1; i <= 10; i++) {
+
+  for (repetir = 1; repetir <= i; repetir++) {
+
+        document.write(repetir);
+    
+  }
+
+  document.write(`<br>`);
+}
