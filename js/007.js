9843b4bcf5e7fc4a9c22503317dc79ccd2c4eb1a
vlobo <valentinlobo@hotmail.com>
2024-08-06 06:10:58 -0300

    resuelto ejercicio 7

---
+++ js/007.js
@@ -0,0 +1,28 @@
+/*
+7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario
+ (no mayor de 50)  de la siguiente forma : (suponiendo que indica 30).
+
+303030303030303030303030303030303030303030303030303030303030
+2929292929292929292929292929292929292929292929292929292929
+28282828282828282828282828282828282828282828282828282828
+…..
+333
+22
+1
+*/
+
+
+
+
+for(let i = 50 ; i>=1 ; i --){
+   
+    for(cantidad = i; cantidad>0; cantidad --){
+
+        document.write(i)
+    }
+
+    document.write("<br>")
+
+
+
+}
